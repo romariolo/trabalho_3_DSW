8fdbdf7b923a1f1f67a3e5fb3f29936c8f33500a
Francisco Romario Rodrigues Lopes <romariolo6199@gmail.com>
2025-08-03 10:34:14 -0300

    Trabalho 3 DSW

--- obj/Debug/net8.0/BibliotecaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BibliotecaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43bf7b5caa451272e65f49726ecac195f9edbe16")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
